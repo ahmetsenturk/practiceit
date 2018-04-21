fa3f5a0f3855e501787dca9063e432af4f8ecee8
ahmetsenturk <35606355+ahmetsenturk@users.noreply.github.com>
2018-04-21 18:39:25 +0300

    Update switchPairs.java
    Another possible solution for the problem

--- chapter14/switchPairs.java
+++ chapter14/switchPairs.java
@@ -6,25 +6,20 @@
 public void switchPairs(Stack<Integer> s) {
     Queue<Integer> q = new LinkedList<Integer>();
         
-    while(!s.isEmpty())
-        q.add(s.pop());
-                        
-    while(!q.isEmpty())
-        s.push(q.remove());
-        
-    while(!s.isEmpty()) {
-        int n1 = s.pop();
-                                                            
-        if(s.isEmpty()) {
-            q.add(n1);
-            break;
-        }
-        
-        int n2 = s.pop();
-        q.add(n2);
-        q.add(n1);
-    }
+    int lastElement = 0;
+    int firstSize = s.size();
+    if(firstSize % 2 != 0) last = s.pop();
     
-    while(!q.isEmpty())
+    while(!s.isEmpty()) q.add(s.pop());
+    
+    while(!q.isEmpty()){
+        int n = q.size() - 2;
+        for(int i = 0; i < n; i++)
+            q.add(q.remove());
+        
         s.push(q.remove());
+        s.push(q.remove());
+    }
+
+    if(firstSize % 2 != 0) s.push(last);
 }
