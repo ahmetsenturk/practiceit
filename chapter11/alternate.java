1cb93be7218cf4d9308ad698354c84c4e4be04f8
Ahmet Şentürk <35606355+ahmetsenturk@users.noreply.github.com>
2018-04-30 15:27:35 +0300

    Update alternate.java

--- chapter11/alternate.java
+++ chapter11/alternate.java
@@ -5,23 +5,19 @@
  * the end. Do not modify the parameter lists passed in.
  */
 public List<Integer> alternate(List<Integer> list1, List<Integer> list2) {
-    List<Integer> list = new LinkedList<Integer>();
-    int size1 = list1.size();
-    int size2 = list2.size();
-    int index1 = 0;
-    int index2 = 0;
-    boolean flag = true;
-
-    while(index1 < size1 || index2 < size2) {
-        if(index1 < size1 && flag) {
-            list.add(list1.get(index1));
-            index1++;
-        } else if(index2 < size2 && !flag) {
-            list.add(list2.get(index2));
-            index2++;
+    
+    List<Integer> result = new ArrayList<>();
+    
+    for(int i = 0; i < Math.max(list1.size(), list2.size()); i++){
+        if(i >= list1.size() && i < list2.size()){
+            result.add(list2.get(i));
+        }else if(i < list1.size() && i >= list2.size()){
+            result.add(list1.get(i));
+        }else{
+            result.add(list1.get(i));
+            result.add(list2.get(i));
         }
-        flag = !flag;
     }
- 
-    return list;
+    
+    return result;
 }
